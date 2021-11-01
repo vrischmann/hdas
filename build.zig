30810020e71fd26034547bb77ef4d469ecf96642
Vincent Rischmann <vincent@rischmann.fr>
2021-11-01 13:32:19 +0100

    use git submodules

--- build.zig
+++ build.zig
@@ -1,5 +1,28 @@
 const std = @import("std");
-const deps = @import("deps.zig");
+
+const package_sqlite = std.build.Pkg{
+    .name = "sqlite",
+    .path = .{ .path = "third_party/zig-sqlite/sqlite.zig" },
+};
+const package_args = std.build.Pkg{
+    .name = "args",
+    .path = .{ .path = "third_party/zig-args/args.zig" },
+};
+const package_prometheus = std.build.Pkg{
+    .name = "prometheus",
+    .path = .{ .path = "third_party/zig-prometheus/src/main.zig" },
+};
+const package_apple_pie = std.build.Pkg{
+    .name = "apple_pie",
+    .path = .{ .path = "third_party/apple_pie/src/apple_pie.zig" },
+};
+
+const packages = &[_]std.build.Pkg{
+    package_sqlite,
+    package_args,
+    package_prometheus,
+    package_apple_pie,
+};
 
 pub fn build(b: *std.build.Builder) void {
     const test_data = b.option([]const u8, "test_data", "Test with this data file");
@@ -8,11 +31,32 @@ pub fn build(b: *std.build.Builder) void {
     target.setGnuLibCVersion(2, 28, 0);
     const mode = b.standardReleaseOptions();
 
+    const sqlite = b.addStaticLibrary("sqlite", null);
+    sqlite.addCSourceFile("third_party/zig-sqlite/c/sqlite3.c", &[_][]const u8{
+        "-std=c99",
+        "-Wall",
+        "-Wextra",
+        "-fsanitize=undefined",
+    });
+    sqlite.linkLibC();
+
+    //
+
     const exe = b.addExecutable("health-data-api", "src/main.zig");
     exe.setTarget(target);
     exe.setBuildMode(mode);
-    deps.addAllTo(exe);
+    exe.addIncludeDir("third_party/zig-sqlite/c");
+
+    inline for (packages) |pkg| {
+        exe.addPackage(pkg);
+    }
+
+    exe.linkLibC();
+    exe.linkLibrary(sqlite);
+
     exe.install();
+
+    //
 
     const run_cmd = exe.run();
     run_cmd.step.dependOn(b.getInstallStep());
