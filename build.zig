79bbd4548b5978506c13100b67918172254c6279
Vincent Rischmann <vincent@rischmann.fr>
2021-11-06 01:16:18 +0100

    build: rename binary

--- build.zig
+++ build.zig
@@ -42,7 +42,7 @@ pub fn build(b: *std.build.Builder) void {
 
     //
 
-    const exe = b.addExecutable("hda", "src/main.zig");
+    const exe = b.addExecutable("hdas", "src/main.zig");
     exe.setTarget(target);
     exe.setBuildMode(mode);
     exe.addIncludeDir("third_party/zig-sqlite/c");
