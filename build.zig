ab5a2f85dde0d7dd82a9de43cbcabf588658098d
Vincent Rischmann <vincent@rischmann.fr>
2022-01-23 03:26:19 +0100

    fix build using musl

--- build.zig
+++ build.zig
@@ -28,10 +28,14 @@ pub fn build(b: *std.build.Builder) void {
     const test_data = b.option([]const u8, "test_data", "Test with this data file");
 
     var target = b.standardTargetOptions(.{});
-    target.setGnuLibCVersion(2, 28, 0);
+    if (target.isGnuLibC()) {
+        target.setGnuLibCVersion(2, 28, 0);
+    }
     const mode = b.standardReleaseOptions();
 
     const sqlite = b.addStaticLibrary("sqlite", null);
+    sqlite.setTarget(target);
+    sqlite.setBuildMode(mode);
     sqlite.addCSourceFile("third_party/zig-sqlite/c/sqlite3.c", &[_][]const u8{
         "-std=c99",
         "-Wall",
