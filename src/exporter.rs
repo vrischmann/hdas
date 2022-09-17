dba7b025496731bbf05c400593c7bd14f9fc43b3
Vincent Rischmann <vincent@rischmann.fr>
2022-09-17 12:29:26 +0200

    migrate to rust

---
+++ src/exporter.rs
@@ -0,0 +1,321 @@
+use crate::db;
+use crate::shutdown::Shutdown;
+use log::{debug, error, info};
+use std::fmt;
+use std::fmt::Write;
+use std::io;
+use std::net;
+use std::sync::Arc;
+use std::time;
+use tokio::io::AsyncWriteExt;
+use tokio::net::TcpStream;
+
+#[derive(Debug)]
+pub enum Error {
+    IO(io::Error),
+    SQLx(sqlx::Error),
+    Fmt(fmt::Error),
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::IO(err) => write!(f, "{}", err),
+            Self::SQLx(err) => write!(f, "{}", err),
+            Self::Fmt(err) => write!(f, "{}", err),
+        }
+    }
+}
+
+impl std::error::Error for Error {}
+
+impl From<io::Error> for Error {
+    fn from(err: io::Error) -> Error {
+        Error::IO(err)
+    }
+}
+
+impl From<sqlx::Error> for Error {
+    fn from(err: sqlx::Error) -> Error {
+        Error::SQLx(err)
+    }
+}
+
+impl From<fmt::Error> for Error {
+    fn from(err: fmt::Error) -> Error {
+        Error::Fmt(err)
+    }
+}
+
+pub type Result<T> = std::result::Result<T, Error>;
+
+pub struct Exporter {
+    db: Arc<db::Db>,
+    addr: net::SocketAddr,
+    stream: Option<TcpStream>,
+}
+
+impl Exporter {
+    pub fn new(db: Arc<db::Db>, addr: net::SocketAddr) -> Self {
+        Self {
+            db,
+            addr,
+            stream: None,
+        }
+    }
+
+    pub async fn run(mut self, mut shutdown: Shutdown) -> Result<()> {
+        let mut interval = tokio::time::interval(time::Duration::from_secs(1));
+
+        'outer_loop: loop {
+            tokio::select! {
+                _ = shutdown.recv() => {
+                    info!("exporter shutting down");
+                    break 'outer_loop;
+                },
+                _ = interval.tick() => {
+                    match self.do_export().await {
+                        Ok(_) => {},
+                        Err(err) => error!("unable to export data, err: {}", err),
+                    }
+                },
+            }
+        }
+
+        match self.stream {
+            Some(stream) => drop(stream),
+            None => {}
+        }
+
+        Ok(())
+    }
+
+    async fn connect(&mut self) -> Result<&mut TcpStream> {
+        match self.stream {
+            Some(ref mut s) => Ok(s),
+            None => {
+                debug!("connecting to {}", self.addr);
+
+                let stream = TcpStream::connect(self.addr).await?;
+                self.stream = Some(stream);
+
+                // Safe because we know it's there
+                Ok(self.stream.as_mut().unwrap())
+            }
+        }
+    }
+
+    async fn do_export(&mut self) -> Result<()> {
+        let mut exported: usize = 0;
+        let mut commands_buffer = String::new();
+
+        // Generate all the export commands
+
+        self.gen_export_heart_rate_commands(&mut commands_buffer, &mut exported)
+            .await?;
+        for metric_type in ALL_GENERIC_METRIC_TYPES {
+            self.gen_export_generic_commands(&mut commands_buffer, metric_type, &mut exported)
+                .await?;
+        }
+        self.gen_export_sleep_analysis_commands(&mut commands_buffer, &mut exported)
+            .await?;
+
+        // Send them to Victoria
+        let stream = self.connect().await?;
+        stream.write_all(commands_buffer.as_bytes()).await?;
+
+        if exported > 0 {
+            info!("exported {} data points", exported);
+        }
+
+        Ok(())
+    }
+
+    async fn gen_export_heart_rate_commands(
+        &mut self,
+        commands_buffer: &mut String,
+        exported: &mut usize,
+    ) -> Result<()> {
+        // Get the data and write the export commands to the buffer
+
+        let rows = sqlx::query!(
+            r#"
+            SELECT d.id, d.max, d.date
+            FROM data_point_heart_rate d
+            INNER JOIN metric m ON d.metric_id = m.id
+            WHERE m.name = 'heart_rate'
+            AND d.exported = 0"#,
+        )
+        .fetch_all(&self.db.pool)
+        .await?;
+
+        let mut ids = Vec::<i64>::new();
+        for row in rows {
+            writeln!(
+                commands_buffer,
+                "put health_data_heart_rate {} {} ",
+                row.date, row.max
+            )?;
+
+            ids.push(row.id);
+        }
+
+        // Mark all data points as exported
+
+        *exported += ids.len();
+
+        let mut tx = self.db.pool.begin().await?;
+        for id in ids {
+            sqlx::query!(
+                r#"
+                UPDATE data_point_heart_rate
+                SET exported = 1
+                WHERE id = ?"#,
+                id,
+            )
+            .execute(&mut tx)
+            .await?;
+        }
+        tx.commit().await?;
+
+        Ok(())
+    }
+
+    async fn gen_export_generic_commands(
+        &mut self,
+        commands_buffer: &mut String,
+        metric_type: GenericMetricType,
+        exported: &mut usize,
+    ) -> Result<()> {
+        let metric_name = metric_type.to_string();
+
+        // Get the data and write the export commands to the buffer
+
+        let rows = sqlx::query!(
+            r#"
+            SELECT d.id, d.quantity, d.date
+            FROM data_point_generic d
+            INNER JOIN metric m ON d.metric_id = m.id
+            WHERE m.name = ?
+            AND d.exported = 0
+            "#,
+            metric_name,
+        )
+        .fetch_all(&self.db.pool)
+        .await?;
+
+        let mut ids = Vec::<i64>::new();
+        for row in rows {
+            writeln!(
+                commands_buffer,
+                "put health_data_{} {} {} ",
+                metric_name, row.date, row.quantity
+            )?;
+
+            ids.push(row.id);
+        }
+
+        // Mark all data points as exported
+
+        *exported += ids.len();
+
+        let mut tx = self.db.pool.begin().await?;
+        for id in ids {
+            sqlx::query!(
+                r#"
+                UPDATE data_point_generic
+                SET exported = 1
+                WHERE id = ?"#,
+                id,
+            )
+            .execute(&mut tx)
+            .await?;
+        }
+        tx.commit().await?;
+
+        Ok(())
+    }
+
+    async fn gen_export_sleep_analysis_commands(
+        &mut self,
+        commands_buffer: &mut String,
+        exported: &mut usize,
+    ) -> Result<()> {
+        // Get the data and write the export commands to the buffer
+
+        let rows = sqlx::query!(
+            r#"
+            SELECT d.id, d.in_bed, d.asleep, d.date
+            FROM data_point_sleep_analysis d
+            INNER JOIN metric m ON d.metric_id = m.id
+            WHERE m.name = 'sleep_analysis'
+            AND d.exported = 0"#,
+        )
+        .fetch_all(&self.db.pool)
+        .await?;
+
+        let mut ids = Vec::<i64>::new();
+        for row in rows {
+            writeln!(
+                commands_buffer,
+                "put health_data_sleep_analysis {} {} type=in_bed",
+                row.date, row.in_bed,
+            )?;
+            writeln!(
+                commands_buffer,
+                "put health_data_sleep_analysis {} {} type=asleep",
+                row.date, row.asleep,
+            )?;
+
+            ids.push(row.id);
+        }
+
+        // Mark all data points as exported
+
+        *exported += ids.len();
+
+        let mut tx = self.db.pool.begin().await?;
+        for id in ids {
+            sqlx::query!(
+                r#"
+                UPDATE data_point_sleep_analysis
+                SET exported = 1
+                WHERE id = ?"#,
+                id,
+            )
+            .execute(&mut tx)
+            .await?;
+        }
+        tx.commit().await?;
+
+        Ok(())
+    }
+}
+
+const ALL_GENERIC_METRIC_TYPES: [GenericMetricType; 5] = [
+    GenericMetricType::WeightBodyMass,
+    GenericMetricType::WalkingHeartRateAverage,
+    GenericMetricType::RestingHeartRate,
+    GenericMetricType::WalkingRunningDistance,
+    GenericMetricType::WalkingSpeed,
+];
+
+enum GenericMetricType {
+    WeightBodyMass,
+    WalkingHeartRateAverage,
+    RestingHeartRate,
+    WalkingRunningDistance,
+    WalkingSpeed,
+}
+
+impl fmt::Display for GenericMetricType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Self::WeightBodyMass => write!(f, "weight_body_mass"),
+            Self::WalkingHeartRateAverage => write!(f, "walking_heart_rate_average"),
+            Self::RestingHeartRate => write!(f, "resting_heart_rate"),
+            Self::WalkingRunningDistance => write!(f, "walking_running_distance"),
+            Self::WalkingSpeed => write!(f, "walking_speed"),
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,190 @@
+
+use axum::handler::Handler;
+use core::future::Future;
+
+use log::{debug, error, info};
+use shutdown::Shutdown;
+use std::net;
+use std::str::FromStr;
+use std::sync::Arc;
+
+
+mod db;
+mod exporter;
+mod health_data;
+mod shutdown;
+mod web;
+
+async fn fallback_handler() -> (http::StatusCode, String) {
+    (http::StatusCode::NOT_FOUND, "Page Not Found".to_owned())
+}
+
+struct App {
+    db_path: String,
+    listen_addr: net::SocketAddr,
+    victoria_addr: net::SocketAddr,
+}
+
+impl App {
+    fn new(db_path: String, listen_addr: net::SocketAddr, victoria_addr: net::SocketAddr) -> Self {
+        Self {
+            db_path,
+            listen_addr,
+            victoria_addr,
+        }
+    }
+
+    async fn run_web_app(
+        db: Arc<db::Db>,
+        listen_addr: net::SocketAddr,
+        mut shutdown: Shutdown,
+    ) -> anyhow::Result<()> {
+        let state = web::State::new(db);
+
+        // Build the router
+        let web_app = axum::Router::new()
+            .route("/health_data", axum::routing::post(web::health_data))
+            .route("/metrics", axum::routing::get(web::metrics))
+            .fallback(fallback_handler.into_service())
+            .layer(tower_http::trace::TraceLayer::new_for_http())
+            .layer(axum::Extension(Arc::new(state)));
+
+        let web_server = axum::Server::bind(&listen_addr)
+            .serve(web_app.into_make_service())
+            .with_graceful_shutdown(shutdown.recv());
+
+        Ok(web_server.await?)
+    }
+
+    async fn run(self, shutdown: impl Future + std::marker::Send + 'static) -> anyhow::Result<()> {
+        // Used for shutdown notinfications
+        let (notify_shutdown_sender, _) = tokio::sync::broadcast::channel(2);
+
+        // Initialize the database
+        let db = Arc::new(db::Db::from_path(&self.db_path).await?);
+
+        // Start the exporter
+        let exporter = exporter::Exporter::new(db.clone(), self.victoria_addr);
+        let exporter_shutdown = Shutdown::new(notify_shutdown_sender.subscribe());
+        let exporter = tokio::task::spawn(exporter.run(exporter_shutdown));
+
+        // Start the web app and web server
+        let web_server_shutdown = Shutdown::new(notify_shutdown_sender.subscribe());
+        let web_server = Self::run_web_app(db.clone(), self.listen_addr, web_server_shutdown);
+
+        // Spawn a task that will notify shutdowns
+        tokio::spawn(async move {
+            shutdown.await;
+            let _result = notify_shutdown_sender.send(()).unwrap();
+        });
+
+        web_server.await?;
+        exporter.await??;
+
+        Ok(())
+    }
+}
+
+fn serve(args: &clap::ArgMatches) -> anyhow::Result<()> {
+    let db_path = args.get_one::<String>("db").unwrap(); // Safe because clap enforces we get a value
+
+    let listen_addr = args
+        .get_one::<String>("listen-addr")
+        .map(|addr| std::net::SocketAddr::from_str(addr).expect("expected a valid listen address"))
+        .unwrap(); // Safe because clap enforces we get a value
+    info!("listen addr: {}", listen_addr);
+
+    let victoria_addr = args
+        .get_one::<String>("victoria-addr")
+        .map(|addr| {
+            std::net::SocketAddr::from_str(addr).expect("expected a valid VictoriaMetrics address")
+        })
+        .unwrap(); // Safe because clap enforces we get a value
+    info!("victoria addr: {}", victoria_addr);
+
+    // Build the Tokio runtime
+
+    let runtime = tokio::runtime::Builder::new_current_thread()
+        .worker_threads(4)
+        .thread_name("hdas")
+        .thread_stack_size(3 * 1024 * 1024)
+        .enable_all()
+        .build()
+        .unwrap();
+    let _runtime_guard = runtime.enter();
+
+    let app = App::new(db_path.to_string(), listen_addr, victoria_addr);
+    let future = app.run(shutdown_signal());
+
+    // Run the app
+    let _ = runtime.block_on(future);
+
+    Ok(())
+}
+
+async fn shutdown_signal() {
+    let ctrl_c = async {
+        tokio::signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+    };
+
+    let terminate = async {
+        tokio::signal::unix::signal(tokio::signal::unix::SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
+
+    debug!("signal received, starting graceful shutdown");
+}
+
+fn main() {
+    let root_command = clap::Command::new("hdas")
+        .version(clap::crate_version!())
+        .about("Health Data API Server")
+        .arg(
+            clap::Arg::new("listen-addr")
+                .long("listen-addr")
+                .help("listening address")
+                .takes_value(true)
+                .default_value("127.0.0.1:5804")
+                .value_name("ADDR"),
+        )
+        .arg(
+            clap::Arg::new("victoria-addr")
+                .long("victoria-addr")
+                .help("Victoria Metrics address")
+                .takes_value(true)
+                .default_value("127.0.0.1:4242")
+                .value_name("ADDR"),
+        )
+        .arg(
+            clap::Arg::new("db")
+                .long("db")
+                .help("database path")
+                .takes_value(true)
+                .default_value("data.db")
+                .value_name("pATH"),
+        );
+
+    let matches = root_command.get_matches();
+
+    // Initialize logger
+    if std::env::var("RUST_LOG").is_err() {
+        std::env::set_var("RUST_LOG", "hdas=debug");
+    }
+    pretty_env_logger::init();
+
+    // Run the appropriate command
+
+    match serve(&matches) {
+        Ok(()) => {}
+        Err(err) => error!("unable to serve, got err {}", err),
+    }
+}
